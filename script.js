78a34969e1307c737ca94fc1b978799413dc16a0
Ajit <yajit1908@gmail.com>
2025-02-22 12:00:49 +0530

    Added the Media Query And Menu bar and close bar Logic

---
+++ script.js
@@ -0,0 +1,13 @@
+//javascript Toggle Menu
+
+var navlinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navlinks.style.right='0';
+
+}
+
+function hideMenu(){
+    navlinks.style.right='-200px';
+    
+}
